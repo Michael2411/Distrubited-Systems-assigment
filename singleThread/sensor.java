0f4b5bc0b78daae52c33dc474f0baaff0db60eb4
Michael Yohanna <73778373+Michael2411@users.noreply.github.com>
2021-11-02 21:04:17 +0200

    Add files via upload

---
+++ singleThread/sensor.java
@@ -0,0 +1,42 @@
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class sensor{
+    public static void main(String[] args){
+        try{
+            ServerSocket sensorServer = new ServerSocket(3000);
+            System.out.println("Sensor Running...");
+            Socket serversocket = sensorServer.accept();
+            DataInputStream sensorIn = new DataInputStream(serversocket.getInputStream());
+            DataOutputStream sensorOut= new DataOutputStream(serversocket.getOutputStream());
+            System.out.println("Sensor Connected");
+            while(true){
+               String Serverinput = sensorIn.readUTF();
+               System.out.println("Client submitted a request");
+               System.out.println("[Client]:Destination is "+ Serverinput);
+
+               String carType = sensorIn.readUTF();
+               System.out.println("[Client]: The Client is Driving a "+carType);
+               if(carType.equalsIgnoreCase("bike")){
+                sensorOut.writeUTF("[App]:The best route is to turn left then right and continue straight forward");
+               }
+               else if(carType.equalsIgnoreCase("car")){
+                sensorOut.writeUTF("[App]:The best route is to continue straight forward and take the bridge");
+               }
+               else if(carType.equalsIgnoreCase("Bus")){
+                sensorOut.writeUTF("[App]:The best route is to continue straight forward and then take the first right before the traffic lights");
+               }
+               
+            }
+        }
+         catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ singleThread/server.java
@@ -0,0 +1,67 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class server {
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            ServerSocket server = new ServerSocket(8000);
+            System.out.println("Server Running...");
+            Socket SensorServer = new Socket("127.0.0.1",3000);
+
+            DataInputStream SensorInput = new DataInputStream(SensorServer.getInputStream());
+            DataOutputStream SensorOutput = new DataOutputStream(SensorServer.getOutputStream());
+            while(true){
+                Socket client = server.accept();
+                DataInputStream clientInput = new DataInputStream(client.getInputStream());
+                DataOutputStream clientOutput = new DataOutputStream(client.getOutputStream());
+                          while (true)
+                    {
+                        clientOutput.writeUTF("[App]:Welcome to the navigator app\nWhere do you want to go ?");
+                        clientOutput.flush();
+                        String location =clientInput.readUTF();
+                        SensorOutput.writeUTF(location);
+                        SensorOutput.flush();
+    
+                        clientOutput.writeUTF("[App]:What are you Driving? (Bus,Car,Bike)");
+                        clientOutput.flush();
+                        String type =clientInput.readUTF();  
+                        SensorOutput.writeUTF(type);
+                        SensorOutput.flush();
+    
+                        String navigation = SensorInput.readUTF();
+                        clientOutput.writeUTF(navigation +"\n[App]:Do you still need additonal help (yes/no)");
+                        clientOutput.flush();
+    
+            
+                        String response =clientInput.readUTF();
+                      
+    
+                        if (response.equalsIgnoreCase("no"))
+                        {
+                            clientOutput.writeUTF("[App]:Thank you for Using our app, Drive Safely");
+                            clientOutput.flush();
+                            break;
+                        }
+                    }
+    
+                    clientInput.close();
+                    clientOutput.close();
+                    client.close();
+                }
+        
+        } catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+
